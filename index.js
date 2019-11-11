4cd8aac7277c0d350e741352c51169966189daca
Gopi Devarapalli <devarapalli.gopi8@gmail.com>
2019-11-11 11:09:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 // Prototype is a way to copy properties of one object to another object. OR we can initialize new property to an Object.
 
 address = 'D.No 1-14, GD,.';
-function Person(name, age)
+function Person(name, age)              // Person is a function
 {
-    this.name = name;
+    this.name = name;               // These are the properties
     this.age = age;
 }
-Person.prototype.address = address;
-Person.prototype.describe = function()
+Person.prototype.address = address;     // we assigning existing property address to the Person 
+Person.prototype.describe = function()  // we assigning new property describe to the Person 
 {
     return this.name + ", " + this.age + " years old. Address"+this.address;
 }
 
- p = new Person('test',23);
-p.describe();
+ p = new Person('John',23);
+p.describe();     //  Output: "John, 23 years old. AddressD.No 1-14, GD,."
 
